18b5fe623ca6d8aaf962f9918218bc9bfafb4453
DawnGate <lam99quyen@gmail.com>
2023-10-30 23:50:44 +1200

    webpack-mf: config tailwindcss for app4

--- webpack/multi-react/app4/src/Widget.jsx
+++ webpack/multi-react/app4/src/Widget.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+
+// 1. First option include all file with your component
+// But it make your host component will change
+// import "tailwindcss/tailwind.css";
+
+// 2. convert to css-in-js
+// using twin.macro,
+
+// 3. using mini-css-extract-plugin, but not what I expected.
+
 import styles from "./styles.module.css";
 
 const Widget = () => {
@@ -12,6 +22,7 @@ const Widget = () => {
       className={styles.widget}
     >
       <h2>This is widget of app 4</h2>
+      <h3 className="bg-teal-200 text-red-300">Test with tailwindcss</h3>
     </div>
   );
 };
