9d20b2bfdb71d5cf0d4f191fcc9edcba9a320245
DawnGate <lam99quyen@gmail.com>
2023-11-29 22:44:48 +1200

    ds-example feat: config and build with rollup - get problem with storybook

---
+++ design-system-ui/appsmith-example/.storybook/main.ts
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]],
+      },
+    });
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    return config;
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+
+export default config;
