d675a2ee3f29f05354dd911cb63342850c4e41de
DawnGate <lam99quyen@gmail.com>
2023-12-04 15:53:11 +1200

    ds-example feat: update theme for storybook ui view and update some docs

---
+++ design-system-ui/appsmith-example/.storybook/AppsmithTheme.ts
@@ -0,0 +1,10 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "Appsmith Design system",
+  brandUrl: "https://www.appsmith.com/",
+  brandImage:
+    "https://global-uploads.webflow.com/61531b23c347e4fbd4a84209/61531b23c347e41e24a8423e_Logo.svg",
+  brandTarget: "_blank",
+});
---
+++ design-system-ui/appsmith-example/.storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import theme from "./AppsmithTheme";
+
+addons.setConfig({
+  theme,
+});
